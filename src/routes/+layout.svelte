3d3383a059779b5f6b9c9858f85020096f9a33da
D2/ERP-la Lambacher <marcel.lambacher@stihl.de>
2024-11-05 09:23:21 +0100

    Add card action bar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '@fortawesome/fontawesome-free/css/all.min.css';
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<main class="h-screen w-screen bg-slate-50">
+	{@render children()}
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import CardActionBar from "./CardActionBar.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<CardActionBar />
