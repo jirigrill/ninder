9705e979713b51b6c946c658d08746140ed2f363
D2/ERP-la Lambacher <marcel.lambacher@stihl.de>
2024-12-11 09:19:13 +0100

    use node adapter

--- src/lib/server/authenticate.ts
+++ src/lib/server/authenticate.ts
@@ -3,11 +3,12 @@ import { FIREBASE_ADMIN_CREDENTIALS } from '$env/static/private';
 import type { RequestEvent } from '@sveltejs/kit';
 
 if (!admin.apps.length) {
-	// admin.initializeApp({
-	// 	credential: admin.credential.cert(
-	// 		JSON.parse(Buffer.from(FIREBASE_ADMIN_CREDENTIALS, 'base64').toString('utf-8'))
-	// 	)
-	// });
+	console.log(FIREBASE_ADMIN_CREDENTIALS);
+	admin.initializeApp({
+		credential: admin.credential.cert(
+			JSON.parse(Buffer.from(FIREBASE_ADMIN_CREDENTIALS, 'base64').toString('utf-8'))
+		)
+	});
 }
 
 export async function authenticate(event: RequestEvent): Promise<string | null> {
